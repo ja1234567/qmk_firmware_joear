bf7e89726bb6db8acc0a65ec716f05ef35926972
Joe Arnold <joearnold12345@hotmail.co.uk>
2025-09-28 16:37:37 +0100

    v1max and q60max now use common/custom_joe macros, added debug keys to v1max

--- keyboards/keychron/v1_max/iso_encoder/keymaps/joear/keymap.c
+++ keyboards/keychron/v1_max/iso_encoder/keymaps/joear/keymap.c
@@ -16,7 +16,7 @@
 
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
-#include "macros.h"
+#include "common/custom_joe/macros.h"
 
 enum layers {
     MAC_BASE,
@@ -53,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_FN] = LAYOUT_iso_83(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,            RGB_TOG,
         _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_END,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  MC_DBCS,  _______,  _______,  _______,  _______,  _______,                      _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  MC_DBJS,  _______,  _______,  _______,  _______,  _______,  _______,            KC_END,
         _______,  _______,  _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______)
 };
@@ -69,9 +69,4 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 };
 #endif // ENCODER_MAP_ENABLE
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!process_record_keychron_common(keycode, record)) {
-        return false;
-    }
-    return true;
-}
+// process_record_user() is handled in common/custom_joe/macros.c
